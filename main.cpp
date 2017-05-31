cd333e75938f683775710d29de06523f6b10bf8a
Kyle <macattackftw@gmail.com>
2017-05-30 23:20:06 -0600

    Plan on keeping all previously calculated available shots based on bow location. Cannot do it with unordered map without writing several functions. For now it will use a vector of pairs<Cube, vector<Cube>>.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 
 /*
 NOTES:
-
+    I'm not sure if storing viable shots is better than recalculating them. Intuition
+    is telling me to keep it as is (save each vector of Cubes).
 
 */
 
@@ -16,6 +17,8 @@ NOTES:
 #include <vector>   // may optimze out
 #include <algorithm>
 #include <climits>
+#include <utility> // pair
+#include <unordered_map>
 
 using namespace std;
 
@@ -41,7 +44,7 @@ enum class Option {STARBOARD, PORT, WAIT, FASTER, SLOWER, FIRE, MINE};
 struct Cube
 {
     Cube () {}
-    Cube (int X, int Y) : x( X - (Y - (Y & 1)) / 2), z(Y), Xo(X), Yo(Y) {y = -x-z;}
+    Cube (int X, int Y) : x( X - (Y - (Y & 1)) / 2), z(Y), Xo(X), Yo(Y) {y = -x - z;}
     Cube (int X, int Y, int Z) : x(X), y(Y), z(Z), Xo(X + (Z - (Z & 1)) / 2), Yo(Z) {}
     int x = INT_MAX;
     int y = INT_MAX;
@@ -52,11 +55,14 @@ struct Cube
 };
 
 
-inline bool operator==(const Cube& lhs, const Cube& rhs){ return lhs.Xo == rhs.Xo && lhs.Yo == rhs.Yo; };
-inline bool operator!=(const Cube& lhs, const Cube& rhs){ return !operator==(lhs,rhs); };
+inline bool operator==(const Cube& lhs, const Cube& rhs) { return lhs.Xo == rhs.Xo && lhs.Yo == rhs.Yo; };
+inline bool operator!=(const Cube& lhs, const Cube& rhs) { return !operator==(lhs, rhs); };
 
 void InFront(Cube &c, int dir);
-vector<Cube> _ShotTemplate;
+vector<Cube> _shot_template;
+// unordered_map<Cube, vector<Cube> > _shot_vectors;
+// _shot_vectors.reserve(400);
+vector<pair<Cube, vector<Cube> > > _shot_vectors;
 void BuildShotTemplate();
 int GetRandomCutoffs(int* cutoffs, int shots_size, int mine, int moves);
 
@@ -86,7 +92,7 @@ class Ship
 {
 public:
     Ship() {}
-    Ship(int X, int Y, int ID, int Direction, int Rum, int Speed) : id(ID), rum(Rum), vec(ShipVec(Cube(X,Y),Direction,Speed)) {}
+    Ship(int X, int Y, int ID, int Direction, int Rum, int Speed) : id(ID), rum(Rum), vec(ShipVec(Cube(X, Y), Direction, Speed)) {}
     Ship(int ID, int Rum, ShipVec Ship_vector) : id(ID), rum(Rum), vec(Ship_vector) {}
     ShipVec vec;
 
@@ -96,7 +102,7 @@ public:
     int rum;
     vector<Action> actions;
     int cutoffs[7];
-
+    vector<Cube> viable_shots;
 
     /*
         Fills the actions vector with all possible actions. Also fills the cutoffs
@@ -107,7 +113,47 @@ public:
         int shots = 0;
         int mines = 0;
         if (sim_turn - fired_last > 1)
-            shots = TranslatePossibleShots(vec.loc, vec.dir, vec.speed);
+        {
+            Cube bow = vec.loc;
+            InFront(bow, vec.dir);
+            bool found = false;
+
+            
+            for (unsigned int i = 0; i < _shot_vectors.size(); ++i)
+            {
+                if (_shot_vectors[i].first == bow)
+                {
+                    found = true;
+                    viable_shots = _shot_vectors[i].second;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                viable_shots = TranslatePossibleShots(vec.loc, vec.dir, vec.speed);
+                _shot_vectors.emplace_back(bow, viable_shots);                
+            }
+
+            // unordered_map<Cube,vector<Cube> >::const_iterator found = _shot_vectors.find (bow);
+            // if (found == _shot_vectors.end())
+            // {
+            //     viable_shots = TranslatePossibleShots(vec.loc, vec.dir, vec.speed);
+            //     _shot_vectors.emplace(bow, viable_shots);
+            // }
+            // else
+            // {
+            //     viable_shots = found->second;
+            // }
+
+            shots = viable_shots.size();
+            Cube new_loc = vec.loc;
+            if (vec.speed > 0)
+                InFront(new_loc, vec.dir);
+            if (vec.speed > 1)
+                InFront(new_loc, vec.dir);
+            for (unsigned int i = 0; i < shots; ++i)
+                actions.emplace_back(ShipVec(new_loc, vec.dir, vec.speed), Option::FIRE, viable_shots[i]);
+        }
         if (sim_turn - mine_dropped > 4)
         {
             PossibleMine();
@@ -126,6 +172,11 @@ public:
     }
 
 private:
+
+    /*
+        Updates actions vector with the possible moves. Returns an integer
+        representing the number of moves.
+    */
     int PossibleMoves()
     {
         Cube new_loc = vec.loc;
@@ -135,8 +186,8 @@ private:
 
         if (vec.speed == 0)
         {
-            ShipVec wait(new_loc,vec.dir, 0);
-            ShipVec starboard(new_loc,new_starboard_dir, 0);
+            ShipVec wait(new_loc, vec.dir, 0);
+            ShipVec starboard(new_loc, new_starboard_dir, 0);
             ShipVec port(new_loc, new_port_dir, 0);
 
             InFront(new_loc, vec.dir);
@@ -150,32 +201,32 @@ private:
         }
         else if (vec.speed == 1)
         {
-            ShipVec slower(new_loc,vec.dir, 0);
+            ShipVec slower(new_loc, vec.dir, 0);
 
             InFront(new_loc, vec.dir);
-            ShipVec wait(new_loc,vec.dir, 1);
-            ShipVec starboard(new_loc,new_starboard_dir, 1);
+            ShipVec wait(new_loc, vec.dir, 1);
+            ShipVec starboard(new_loc, new_starboard_dir, 1);
             ShipVec port(new_loc, new_port_dir, 1);
 
             InFront(new_loc, vec.dir);
             ShipVec faster(new_loc, vec.dir, 2);
-            
+
             actions.emplace_back(wait, Option::WAIT);
             actions.emplace_back(slower, Option::SLOWER);
             actions.emplace_back(starboard, Option::STARBOARD);
             actions.emplace_back(port, Option::PORT);
-            actions.emplace_back(faster, Option::FASTER);            
+            actions.emplace_back(faster, Option::FASTER);
             return 5;
         }
         else
         {
             InFront(new_loc, vec.dir);
-            ShipVec slower(new_loc,vec.dir, 1);
+            ShipVec slower(new_loc, vec.dir, 1);
 
             InFront(new_loc, vec.dir);
-            ShipVec wait(new_loc,vec.dir, 2);
-            ShipVec starboard(new_loc,new_starboard_dir, 2);
-            ShipVec port(new_loc, new_port_dir, 2);   
+            ShipVec wait(new_loc, vec.dir, 2);
+            ShipVec starboard(new_loc, new_starboard_dir, 2);
+            ShipVec port(new_loc, new_port_dir, 2);
 
             actions.emplace_back(wait, Option::WAIT);
             actions.emplace_back(slower, Option::SLOWER);
@@ -186,32 +237,24 @@ private:
     }
 
     /*
-        Translates each Cube in _ShotTemplate to be based on the new bow.
+        Translates each Cube in _shot_template to be based on the new bow.
         Only adds Cubes that are in the map boundry.
     */
-    int TranslatePossibleShots(Cube center, int dir, int speed)
+    vector<Cube> TranslatePossibleShots(Cube center, int dir, int speed)
     {
-        int ret_val = 0;
-        Cube new_loc = center;
-        if (speed > 0)
-            InFront(new_loc, dir);
-        if (speed > 1)
-            InFront(new_loc, dir);
+        vector<Cube> ret_val;
 
         // Shoot from bow...so move the center forward one to the bow
         InFront(center, dir);
         int dx = 1000 - center.x;
         int dy = 1000 - center.y;
         int dz = 1000 - center.z;
-        // 331 = _ShotTemplate.size()
+        // 331 = _shot_template.size()
         for (unsigned int i = 0; i < 331; ++i)
         {
-            Cube cube(_ShotTemplate[i].x - dx, _ShotTemplate[i].y - dy, _ShotTemplate[i].z - dz);
+            Cube cube(_shot_template[i].x - dx, _shot_template[i].y - dy, _shot_template[i].z - dz);
             if (cube.Xo >= 0 && cube.Xo < 23 && cube.Yo >= 0 && cube.Yo < 21)
-            {
-                actions.emplace_back(ShipVec(new_loc, dir, speed), Option::FIRE, cube);
-                ++ret_val;
-            }
+                ret_val.emplace_back(cube);
         }
         return ret_val;
     }
@@ -232,10 +275,10 @@ private:
 
     /*
         shots_size: 0-331   mine: 0 or 1
-        The cutoffs array stores cutoffs. I wanted at least 2 of the 5 actions to be 
+        The cutoffs array stores cutoffs. I wanted at least 2 of the 5 actions to be
         movement related. Since there are 331 possible shots and only 5 possible moves
-        it is necessary to represent them as a percentage of total. I assume shots 
-        make up 55% of the pool, mines 5%, and movement the remainder. In the event 
+        it is necessary to represent them as a percentage of total. I assume shots
+        make up 55% of the pool, mines 5%, and movement the remainder. In the event
         there are no possible shots (fire last round), or mines, the function handles
         that elegantly (0 * anything = 0, 0 + anything = anything).
 
@@ -245,7 +288,6 @@ private:
     {
 
         float total = shots_size == 0 ? 100.0 : shots_size / .55;
-
         float mine_tot = mine * total * .05;
         float remainder = total - shots_size - mine_tot;
         float move_tot = remainder / moves;
@@ -259,7 +301,7 @@ private:
             running_total += move_tot;
             cutoffs[2] = int(running_total);
         }
-        
+
         return running_total;
     }
 };
@@ -267,10 +309,65 @@ vector<Ship> _my_ships;
 vector<Ship> _en_ships;
 
 
+
+class GA
+{
+public:
+    GA()
+    {
+        my_ships = _my_ships;
+        en_ships = _en_ships;
+    }
+
+    void FillShips()
+    {
+        for (unsigned int i = 0; i < 5; ++i)
+        {
+            for (unsigned int j = 0; j < my_ships.size(); ++j)
+            {
+
+            }
+            for (unsigned int j = 0; j < en_ships.size(); ++j)
+            {
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    vector<Ship> my_ships;
+    vector<Ship> en_ships;
+    // Generate the random arrangements but save unique generations of shot vectors
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct Barrel
 {
     Barrel() {}
-    Barrel(int X, int Y, int Rum, int ID) : loc(Cube(X,Y)), rum(Rum), id(ID) {}
+    Barrel(int X, int Y, int Rum, int ID) : loc(Cube(X, Y)), rum(Rum), id(ID) {}
     Cube loc;
 
     int id = -1;
@@ -283,7 +380,7 @@ vector<Barrel> _barrels;
 struct Cannonball
 {
     Cannonball() {}
-    Cannonball(int X, int Y, int Impact, int Turn, int ID) : loc(Cube(X,Y)), impact(Impact), turn(Turn), id(ID) {}
+    Cannonball(int X, int Y, int Impact, int Turn, int ID) : loc(Cube(X, Y)), impact(Impact), turn(Turn), id(ID) {}
     Cube loc;
 
     int turn;
@@ -296,7 +393,7 @@ vector<Cannonball> _cbs;
 struct Mine
 {
     Mine() {}
-    Mine(int X, int Y, int ID) : loc(Cube(X,Y)), id(ID) {}
+    Mine(int X, int Y, int ID) : loc(Cube(X, Y)), id(ID) {}
     Cube loc;
 
     int id = -1;
@@ -311,7 +408,7 @@ int main()
     BuildShotTemplate();
 
     while (1) {
-        vector<Ship> my_ships,en_ships;
+        vector<Ship> my_ships, en_ships;
         int myShipCount; // the number of remaining ships
         cin >> myShipCount; cin.ignore();
         int entityCount; // the number of entities (e.g. ships, mines or cannonballs)
@@ -329,7 +426,7 @@ int main()
             bool found = false;
             if (entityType == "SHIP")
             {
-                Ship new_ship(x,y, entityId,arg1,arg3,arg2);                
+                Ship new_ship(x, y, entityId, arg1, arg3, arg2);
                 if (arg4 == 1)
                 {
                     for (unsigned int j = 0; j < _my_ships.size(); ++j)
@@ -340,7 +437,7 @@ int main()
                             _my_ships[j].vec.loc = new_ship.vec.loc;
                             _my_ships[j].vec.dir = new_ship.vec.dir;
                             _my_ships[j].vec.speed = new_ship.vec.speed;
-                            _my_ships[j].rum = new_ship.rum;                            
+                            _my_ships[j].rum = new_ship.rum;
                         }
                     }
                     if (!found)
@@ -357,7 +454,7 @@ int main()
                             _en_ships[j].vec.loc = new_ship.vec.loc;
                             _en_ships[j].vec.dir = new_ship.vec.dir;
                             _en_ships[j].vec.speed = new_ship.vec.speed;
-                            _en_ships[j].rum = new_ship.rum;                            
+                            _en_ships[j].rum = new_ship.rum;
                         }
                     }
                     if (!found)
@@ -366,7 +463,7 @@ int main()
             }
             else if (entityType == "BARREL")
             {
-                Barrel new_barrel(x,y,arg1,entityId);
+                Barrel new_barrel(x, y, arg1, entityId);
                 for (unsigned int j = 0; j < _barrels.size(); ++j)
                 {
                     if (_barrels[j].id == new_barrel.id)
@@ -377,11 +474,11 @@ int main()
                     }
                 }
                 if (!found)
-                    _barrels.emplace_back(new_barrel);                
+                    _barrels.emplace_back(new_barrel);
             }
             else if (entityType == "CANNONBALL")
             {
-                Cannonball new_cb(x,y,arg2,_turn,entityId);
+                Cannonball new_cb(x, y, arg2, _turn, entityId);
                 for (unsigned int j = 0; j < _cbs.size(); ++j)
                     if (_cbs[j].id == new_cb.id)
                         found = true;
@@ -391,18 +488,18 @@ int main()
                     for (unsigned int j = 0; j < _en_ships.size(); ++j)
                         if (arg2 == _en_ships[j].id)
                             new_cb.origin = &_en_ships[j];     // Update now or later??? (ship that launched)
-                    _cbs.emplace_back(new_cb);                    
+                    _cbs.emplace_back(new_cb);
                 }
             }
             else if (entityType == "MINE")
             {
-                Mine new_mine(x,y,entityId);
+                Mine new_mine(x, y, entityId);
                 for (unsigned int j = 0; j < _mines.size(); ++j)
                     if (_mines[j].id == new_mine.id)
                         found = true;
 
                 if (!found)
-                    _mines.emplace_back(new_mine);                    
+                    _mines.emplace_back(new_mine);
             }
 
         }
@@ -420,19 +517,19 @@ int main()
     return 0;
 }
 
-/*  
+/*
     Fills the vector of all hexes within a radius of a point. Generates 331
     Cubes (shot locations)
 */
 void BuildShotTemplate()
 {
-    Cube bow(1000,1000,1000);
+    Cube bow(1000, 1000, 1000);
     // Can be optimized:
     for (unsigned int i = -10 + bow.x; i <= 10 + bow.x; ++i)
         for (unsigned int j = -10 + bow.y; j <= 10 + bow.y; ++j)
             for (unsigned int k = -10 + bow.z; k <= 10 + bow.z; ++k)
                 if (i + j + k == 0)
-                        _ShotTemplate.emplace_back(i,j,k);
+                    _shot_template.emplace_back(i, j, k);
 }
 
 
